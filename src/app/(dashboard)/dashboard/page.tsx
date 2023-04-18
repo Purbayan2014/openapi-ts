49ee955739d0df872c6e34eb7f8ae901f3a4919d
purbayan2014 <purbayan2014@gmail.com>
2023-04-18 09:56:20 +0530

    api dashboard structure implemented

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+
+import type { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { notFound } from "next/navigation";
+import { db } from "@/lib/db";
+import ApiDashBoard from '@/components/ApiDashBoard'
+import RequestApiKey from '@/components/RequestApiKey'
+
+export const metadata: Metadata = {
+    title: 'Plagarism API | Home',
+    description: 'Free & open-source Plagarism checking API',
+}
+
+interface PageProps {}
+
+const Page = async () => {
+
+    const user = await getServerSession(authOptions)
+    if(!user) {
+        return notFound()
+    }
+
+    const apiKey = await db.apiKey.findFirst({
+        where : {
+            userId: user.user.id, enabled: true
+        },
+    })
+
+    return (
+        <div className="max-w-7xl max-auto mt-16">
+            {
+                apiKey ? (
+                    <ApiDashBoard />):
+                    (
+                     <RequestApiKey/>
+                    )
+            }
+        </div>
+    )
+}
+
+
+export default Page;
---
+++ src/components/ApiDashBoard.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react'
+
+interface ApiDashBoard {
+
+}
+
+const ApiDashBoard: FC<ApiDashBoard> = () => {
+    return (
+        <div>
+            <h1>ApiDashBoard</h1>
+        </div>
+    )
+}
+
+
+export default ApiDashBoard
